b911905d9371c939fda59aded628a46dcd057f13
Evgeniy Bortsov <ebortsoff@yandex.ru>
2024-06-30 21:29:14 +0300

    Added parcel bundler. Reformated the whole project

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface ComicResponse {
+    month: string;
+    num: number;
+    link: string;
+    year: string;
+    news: string;
+    safe_title: string;
+    transcript: string;
+    alt: string;
+    img: string;
+    title: string;
+    day: string;
+}
+
+export interface XKCDIdResponse {
+    id: number;
+}
